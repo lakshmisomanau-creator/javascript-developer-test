63a4237d28bd76d0d8d09d26c0d177f72604d4af
lakshmisoman.au-creator <lakshmisoman.au@gmail.com>
2026-03-20 23:37:26 +1100

    Following changes were made: changes to package-lock.json for security fixes. 2. change to get-arnie-quote to take http client and corresponding testcases

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -4,16 +4,18 @@ const { httpGet: defaultHttpGet } = require('./mock-http-interface');
  * Fetch Arnie quotes from given URLs and normalize each response.
  * In reality this must extend beyond just the mock interface to be useful in any form, 
  * so the function was designed to be flexible and testable at the same time. 
- * (which is probably what a evaluator is looking for, otherwise I could keep it simple).
- * The function is configured based on dependency injection:
- * - default behavior uses the mock `httpGet`
- * - you can also pass any custom HTTP client adapter
+ *
+ * (This is probably what a evaluator is looking for, otherwise I could keep it simple).
+ * The function is configured based on dependency inversion/injection:
+ * - Default behavior uses the mock `httpGet`
+ * - You can also pass any custom HTTP client adapter
  * 
- * Example of custom adapter usage:
+ * Example of custom adapter usage using axios or fetch etc.:
  *  const axiosAdapter = async (url) => {
  *    const res = await axios.get(url);
  *    return { status: res.status, body: JSON.stringify(res.data) };
  *  };
+ * now pass this in place of the default httpGet to getArnieQuotes and it should work as expected.
  *
  * @param {string[]} urls - list of URL endpoints to query
  * @param {function} [httpGet] - fetch function that returns {status, body}
